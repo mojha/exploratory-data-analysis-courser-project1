18bea275647560c82b2c04f58373737e1101d323
mojha <mojha2008@gmail.com>
2016-04-04 01:51:15 -0400

    fifth commit

--- plot4.R
+++ plot4.R
@@ -12,6 +12,8 @@ rm(house_data)
 date_and_time<-paste(as.Date(house_data_full$Date),house_data_full$Time)
 house_data_full$Datetime<-as.POSIXct(date_and_time)
 #plot number 4
+# Save file to plot4.png
+png("plot4.png", width=480, height=480)
 par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
 with(house_data_full, {
         plot(Global_active_power~Datetime, type="l",
@@ -22,14 +24,13 @@ with(house_data_full, {
              ylab="Global Active Power", xlab="")
         lines(Sub_metering_2~Datetime,col='Red')
         lines(Sub_metering_3~Datetime,col='Blue')
-        legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2,bty="n",
+        legend("topright",col=c("black", "red", "blue"), lty=1, lwd=2,bty="n",
                legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
         plot(Global_reactive_power~Datetime, type="l",
              ylab="Global_reactive_power",xlab="datetime")
 })
-# Save the plot
+# Turn the png device off
 
-dev.copy(png, file="plot4.png", height=480, width=480)
 dev.off()
 
 

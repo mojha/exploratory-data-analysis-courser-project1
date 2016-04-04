93f4a7d267bce475b82a3e4575b841c09bd30243
mojha <mojha2008@gmail.com>
2016-04-04 00:11:44 -0400

    fourth commit

--- plot4.R
+++ plot4.R
@@ -8,28 +8,27 @@ house_data$Date<-as.Date(house_data$Date,format="%d/%m/%Y")
 house_data_full<-subset(house_data, 
                         subset=(Date>="2007-02-01"&Date<="2007-02-02"))
 rm(house_data)
-##convert dates
+##converting dates
 date_and_time<-paste(as.Date(house_data_full$Date),house_data_full$Time)
 house_data_full$Datetime<-as.POSIXct(date_and_time)
 #plot number 4
 par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
 with(house_data_full, {
         plot(Global_active_power~Datetime, type="l",
-             ylab="Global Active Power (kilowatts)", xlab="")
+             ylab="Global Active Power", xlab="")
         plot(Voltage~Datetime, type="l",
-             ylab="Voltage (volt)", xlab="")
+             ylab="Voltage", xlab="datetime")
         plot(Sub_metering_1~Datetime, type="l",
              ylab="Global Active Power (kilowatts)", xlab="")
         lines(Sub_metering_2~Datetime,col='Red')
         lines(Sub_metering_3~Datetime,col='Blue')
-        legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
+        legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2,
                legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
         plot(Global_reactive_power~Datetime, type="l",
-             ylab="Global Rective Power (kilowatts)",xlab="")
+             ylab="Global_reactive_power",xlab="datetime")
 })
-# Save the plot to a png file
+# Save the plot
 dev.copy(png, file="plot4.png", height=480, width=480)
-#Turn the png device off
 dev.off()
 
 

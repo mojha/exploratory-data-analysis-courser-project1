5316be4029aa901a52a8d4bbfea36d6ac263ebea
mojha <mojha2008@gmail.com>
2016-04-04 00:27:12 -0400

    fourth commit

--- plot4.R
+++ plot4.R
@@ -22,7 +22,7 @@ with(house_data_full, {
              ylab="Global Active Power", xlab="")
         lines(Sub_metering_2~Datetime,col='Red')
         lines(Sub_metering_3~Datetime,col='Blue')
-        legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2,
+        legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2,bty="n",
                legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
         plot(Global_reactive_power~Datetime, type="l",
              ylab="Global_reactive_power",xlab="datetime")
